ea633557e66c1837ccb49472e1d52d2e2d8b963a
Mukul <mukul1singh23@gmail.com>
2024-07-19 13:21:34 +0530

    google sign up

--- Backend/passoprt.js
+++ Backend/passoprt.js
@@ -23,7 +23,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "/auth/google/callback",
+      callbackURL: "https://recipe-community-server.vercel.app/google/callback",
     },
     async (req, accessToken, refreshToken, profile, done) => {
       // console.log("Google profile:", profile); // Log profile object
