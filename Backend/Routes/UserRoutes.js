133246f1d8eb533121acdcde9181decfe6b8ff39
Mukul <mukul1singh23@gmail.com>
2024-08-18 22:05:58 +0530

    set login success route

--- Backend/Routes/UserRoutes.js
+++ Backend/Routes/UserRoutes.js
@@ -171,6 +171,8 @@ router.post('/login', passport.authenticate('local',{  session: true
       // console.log("inside login routs  " + req.user);
       console.log("req.user in login")
       console.log(req.user)
+      console.log('Session:', req.session); 
+      console.log('Cookies:', req.cookies); 
       return res.status(200).json({ message: 'Logged in successfully', user:req.user });   
     }
     catch(e)
