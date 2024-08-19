7cdf2e0861a752e87e2429734f97e7ca99cd0aa2
Mukul <mukul1singh23@gmail.com>
2024-08-19 23:12:34 +0530

    final

--- Backend/index.js
+++ Backend/index.js
@@ -51,10 +51,10 @@ app.use(
   })
 );
 
-app.use((req, res, next) => {
-  res.set("Cache-Control", "no-cache, no-store, must-revalidate");
-  next();
-});
+// app.use((req, res, next) => {
+//   res.set("Cache-Control", "no-cache, no-store, must-revalidate");
+//   next();
+// });
 
 
 // app.use(session({
